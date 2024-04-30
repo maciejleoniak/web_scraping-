c7cf97231ef4411bcb65df3b08299d4f18ec5d57
7799043 <leoniak.maciek1@gmail.com>
2024-04-30 22:25:36 +0200

    data storage in csv

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from scraper_machine import scrape_and_save_offers
 from src.hosts import url1, url2
+from src.csv_file_path import csv_file_path
 
 def main():
     urls = [url1, url2]
     for url in urls:
-        scrape_and_save_offers(url)
+        scrape_and_save_offers(url, csv_file_path)
 
 if __name__ == "__main__":
     main()
--- scraper_machine.py
+++ scraper_machine.py
@@ -1,11 +1,12 @@
+import csv
+import os.path
+
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from datetime import datetime
 
 
-def scrape_and_save_offers(url):
-
-
+def scrape_and_save_offers(url, csv_file_path):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("detach", True)
 
@@ -18,47 +19,51 @@ def scrape_and_save_offers(url):
         policy_accept.click()
 
         # Find elements containing single offer price, description, and link
-        price = driver.find_elements(By.CSS_SELECTOR,
-                                                  "div[data-testid='listing-item-header'] span.e1a3ad6s0")
-        street = driver.find_elements(By.CSS_SELECTOR, "p[data-testid='advert-card-address']")
-        desc = driver.find_elements(By.CSS_SELECTOR, "a[data-testid='listing-item-link']")
+        prices = driver.find_elements(By.CSS_SELECTOR, "div[data-testid='listing-item-header'] span.e1a3ad6s0")
+        streets = driver.find_elements(By.CSS_SELECTOR, "p[data-testid='advert-card-address']")
+        descs = driver.find_elements(By.CSS_SELECTOR, "a[data-testid='listing-item-link']")
 
         # Print lengths for debugging
-        print(len(price))
-        print(len(street))
-        print(len(desc))
+        print(len(prices))
+        print(len(streets))
+        print(len(descs))
 
-        if len(price) > 0:
+        if len(prices) > 0:
             # Prepare data for saving
-            all_offers = {}
-            for n in range(len(price)):
-                all_offers[n] = {
-                    "price": price[n].text,
-                    "street": street[n].text,
-                    "desc": desc[n].text,
-                    "link": desc[n].get_attribute("href")
-                }
-
-            # Convert to string
-            str_to_save = str(all_offers)
-
-            # Get the current date and time
+            all_offers = []
             current_datetime = datetime.now()
+            current_datetime_str = current_datetime.strftime("%Y-%m-%d")
+            for price, street, desc in zip(prices, streets, descs):
+                offer = {
+                    "scraped_date": current_datetime_str,
+                    "price": price.text,
+                    "street": street.text,
+                    "desc": desc.text,
+                    "link": desc.get_attribute("href"),
+                }
+                all_offers.append(offer)
 
-            # Convert the datetime object to a string
-            current_datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            # Define column names
+            fieldnames = ["scraped_date", "price", "street", "desc", "link"]
 
-            # Append the current date and time along with the data to save
-            data_to_save = f"{current_datetime_str}: {str_to_save}"
+            # Check if CSV file exists
+            file_exists = os.path.isfile(csv_file_path)
 
-            # Save to file
-            with open("scraped/my_data.txt", mode="a") as file:
-                file.write("\n" + data_to_save)
+            # Save data to CSV file
+            with open(csv_file_path, mode="a", newline="", encoding="utf-8") as csv_file:
+                writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                if not file_exists:
+                    writer.writeheader() 
+                writer.writerows(all_offers)
+
+            print(f"Data appended to: {csv_file_path}")
         else:
             print("No offers found.")
             driver.quit()
-            scrape_and_save_offers(url)
+            scrape_and_save_offers(url, csv_file_path)
 
     finally:
         # Close the WebDriver
         driver.quit()
+
+
